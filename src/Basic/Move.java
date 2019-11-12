3a3ac986ee0dfe58f194ffec6ee5d0c8e9c91db7
ga4g18 <george@alexiou.xyz>
2019-11-12 04:10:52 +0000

    Added comments to Move class.

--- src/Basic/Move.java
+++ src/Basic/Move.java
@@ -1,5 +1,9 @@
 package Basic;
 
+/**
+ * Enum representing the types of moves that a block can make inside a grid.
+ * The block can move up, down, left and right.
+ */
 public enum Move {
     UP,
     DOWN,
