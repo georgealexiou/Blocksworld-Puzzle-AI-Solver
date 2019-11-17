18651f735ae4ae55d86b6b78a69f6a8b8b744efb
ga4g18 <george@alexiou.xyz>
2019-11-17 06:50:04 +0000

    Renamed files

---
+++ searchMethods.py
@@ -0,0 +1,75 @@
+from FoundationClasses import Node
+from FoundationClasses import State
+from FoundationClasses import Block
+
+import timeit
+import queue
+
+class SearchMethods:
+
+	def __init__(self, startNode, finishState):
+		self.startNode = startNode
+		self.finishState = finishState
+		self.path = []
+
+
+	def BFS(self):
+		path = []
+		startTime = timeit.timeit()
+
+		#initialize queue and dictionary and add Start Node to it
+		q = queue.Queue()
+		q.put(self.startNode)
+
+		visited = {self.startNode: True}
+
+		while not q.empty():
+			#pops first element from the queue and checks what moves can be made
+			currentNode = q.get()
+			currentNode.state.printGrid()
+			possibleMoves = currentNode.checkPossibleMoves()
+
+			if(currentNode.state.isEqual(self.finishState)):
+				time = timeit.timeit() - startTime
+				self.printResults('Breadth First', currentNode, visited, time)
+				quit();
+
+			else:
+				for nextNode in possibleMoves:
+					if possibleMoves != None:
+						q.put(nextNode)
+						visited[nextNode] = True
+
+	def DFS(self):
+		return 0
+
+	def iterativeDeepening(self):
+		return 0
+
+	def aStar(self):
+		return 0
+
+	def printResults(self, searchType, currentNode, visited, time):
+		print ('{} Search Complete'.format(searchType))
+		
+		print ('Start State:')
+		self.startNode.state.printGrid()
+		print ('\nFinal State:')
+		currentNode.state.printGrid()
+
+		print ('\nDepth: {}\nHeuristic Estimate: {}\nMoves Performed: {}\nAmount visited: {}\nTime Taken: {}'.format(currentNode.depth, self.startNode.getHeuristicEstimate(currentNode), currentNode.getPath(), len(visited), time))
+
+a = Block('A', 0, 0)
+b = Block('B', 1, 0)
+c = Block('C', 2, 0)
+agent = Block('P', 3, 0)
+state = State(a, b, c, agent)
+node = Node(state, None, None)
+
+agent1 = Block('P', 3, 3)
+a1 = Block('A', 1, 0)
+b1 = Block('B', 2, 0)
+c1 = Block('C', 3, 0)
+
+sm = SearchMethods(node, State(a1, b1, c1, agent1))
+sm.BFS()
