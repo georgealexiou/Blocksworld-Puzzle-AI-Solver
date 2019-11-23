3624fb2cf0cd54bff4cda08f1a3a439e08d0e3e0
ga4g18 <george@alexiou.xyz>
2019-11-23 23:47:15 +0000

    Modified driver.py

--- driver.py
+++ driver.py
@@ -6,4 +6,24 @@ from searchMethods import SearchMethods
 
 #Initialize Start Node
 startMovables = []
-startMovables.append()
+startMovables.append(Block('A', 1, 0))
+startMovables.append(Block('B', 1, 1))
+startMovables.append(Block('C', 1, 2))
+
+startAgent = Block('P', 2,1)
+startState = State(startMovables, startAgent, None)
+
+startNode = Node(startState, None, None)
+
+#Initialize Goal State
+finMovables = []
+finMovables.append(Block('A', 1, 0))
+finMovables.append(Block('B', 2, 1))
+finMovables.append(Block('C', 1, 2))
+
+finAgent = Block('P', 1,1)
+
+finState = State(finMovables, finAgent, None)
+
+sm = SearchMethods(startNode, finState)
+sm.DFS()
